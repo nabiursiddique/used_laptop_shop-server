9f5becd1eb7f0f633b57013f68b3acb8f6887b59
nabiursiddique <nabiursiddique01@gmail.com>
2023-07-13 02:05:32 +0600

    feature- Now we are storing the created user

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const express = require('express');
-const cors =require('cors');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
-const app =express();
+const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async(req,res)=>{
+// Mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7t8vw3l.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const userCollection = client.db('usedLaptopShop').collection('users');
+
+        // saving users information in the database
+        app.post('/allUsers', async(req,res)=>{
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', async (req, res) => {
     res.send("server is running");
 });
 
-app.listen(port, ()=>console.log(`used laptop server is running on port ${port}`));
+app.listen(port, () => console.log(`used laptop server is running on port ${port}`));
